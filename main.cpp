dd9c7b7b537b2569bcc5a6f527fac73139882e85
Code5150 <f>
2022-05-18 18:31:28 +0400

    Working MPI version; switch between sequential and MPI modes

--- main.cpp
+++ main.cpp
@@ -1,28 +1,20 @@
+//#define DEBUG_PRINT
+//#define NON_PARALLEL
+#define PARALLEL_MPI
+
 #include <cstdio>
 #include "mpi.h"
 #include <vector>
 #include "myutils.hpp"
 #include <numeric>
-#include <Eigen/Dense>
 #include <random>
-#include <iostream>
 #include <chrono>
 
 using std::vector;
-
-template<typename T>
-using Matrix3Trm = Eigen::Matrix<T, 3, 3, Eigen::RowMajor>;
-template<typename T>
-using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-
 using mnum_t = int;
 using hr_clock = std::chrono::high_resolution_clock;
 
-//#define DEBUG_PRINT
-//#define NON_PARALLEL
-#define PARALLEL_MPI
-
-mnum_t sequential_3coef(int n, int coeff_num, const vector<mnum_t> &coefficients, mnum_t x0, mnum_t x_min1) {
+mnum_t sequential_3coef(int n, int coeff_num, const mnum_t* coefficients, mnum_t x0, mnum_t x_min1) {
     mnum_t x = 0;
     auto x_prev2 = x_min1;
     auto x_prev1 = x0;
@@ -117,13 +109,16 @@ int main() {
     std::cout << "Matrix duration: " << mmul_time_avg << "ms\n" << std::endl;
 #endif
 #ifdef PARALLEL_MPI
+
     int rank, size;
     MPI_Init(nullptr, nullptr);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int n = 0, x0 = 0, x1 = 0;
-    mnum_t *sendbuf;
+    mnum_t seq_result;
+    mnum_t *sendbuf = nullptr;
+    mnum_t *x0_vec = nullptr;
     vector<int> count(size);
     vector<int> displ(size);
     if (rank == 0) {
@@ -134,7 +129,7 @@ int main() {
         printf_s("Enter x1:\n");
         scanf_s("%d", &x1);
 
-        auto x0_vec = new mnum_t[matrix_size]();
+        x0_vec = new mnum_t[matrix_size]();
         x0_vec[0] = x0;
         x0_vec[3] = x1;
         x0_vec[6] = 1;
@@ -145,30 +140,108 @@ int main() {
         std::normal_distribution<> dist{10.0, 2.0};
 
         count = count_indices(n, size);
+        std::transform(count.begin(),  count.end(), count.begin(), [](int i){ return i*matrix_size; });
         displ = count_displ(n, size);
+        std::transform(displ.begin(),  displ.end(), displ.begin(), [](int i){ return i*matrix_size; });
 
         auto coefficients = generate_coefficients<mnum_t>(
                 n, matrix_dim, [&gen, &dist]() { return static_cast<mnum_t>(dist(gen)); }
         );
 
         auto t1 = hr_clock::now();
-        auto seq_result = sequential_3coef(n, matrix_dim, coefficients, x0, x1);
+        seq_result = sequential_3coef(n, matrix_dim, coefficients.data(), x0, x1);
         auto t2 = hr_clock::now();
         std::chrono::duration<double, std::milli> ms_double = t2 - t1;
-        printf("Result: %d", seq_result);
-        printf("Time: %.3f ms", ms_double.count());
+        printf("Result: %d\n", seq_result);
+        printf("Time: %.6f ms\n", ms_double.count());
+        printf("\n");
 
         sendbuf = generate_matrices(n, matrix_dim, coefficients.data());
+#ifdef DEBUG_PRINT
+        for (int i = 0; i < matrix_dim; ++i){
+            for (int j = 0; j < n*matrix_dim; ++j) {
+                printf("%-2d ", sendbuf[i*n*matrix_dim + j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+#endif
     }
 
     MPI_Bcast(count.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(displ.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
 
+    const int recvbuf_size = count[rank];
+    auto recvbuf = new mnum_t[recvbuf_size]();
+    auto task_results = new mnum_t[matrix_size * size]();
 
+    double mmul_start = MPI_Wtime();
+    MPI_Scatterv(sendbuf, count.data(), displ.data(), MPI_INT, recvbuf, recvbuf_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 1) {
+#ifdef DEBUG_PRINT
+        for (int i = 0; i < matrix_dim; ++i){
+            for (int j = 0; j < n*matrix_dim; ++j) {
+                printf("%-2d ", sendbuf[i*n*matrix_dim + j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+#endif
+    }
+
+    auto task_result = new mnum_t[matrix_size];
+    std::copy(recvbuf + recvbuf_size - matrix_size, recvbuf + recvbuf_size, task_result);
+    if (count[rank] > 1) {
+        for (int i = recvbuf_size - 2 * matrix_size; i > -1; i -= matrix_size) {
+            auto tmp_result = gemm(matrix_dim, matrix_dim, matrix_dim, task_result, recvbuf + i);
+            auto swap = task_result;
+            task_result = tmp_result;
+            delete[] swap;
+        }
+    }
+
+    MPI_Gather(task_result, matrix_size, MPI_INT, task_results, matrix_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    auto result = new mnum_t[matrix_size];
+    if (rank == 0) {
+        std::copy(task_results + matrix_size * (size - 1), task_results + matrix_size * size, result);
+        if (size > 1) {
+            for (int i = matrix_size * (size - 2); i > -1; i -= matrix_size) {
+                auto tmp_result = gemm(matrix_dim, matrix_dim, matrix_dim, result, task_results + i);
+                auto swap = result;
+                result = tmp_result;
+                delete[] swap;
+            }
+        }
+
+        auto tmp_result = gemm(matrix_dim, matrix_dim, matrix_dim, result, x0_vec);
+        auto swap = result;
+        result = tmp_result;
+        delete[] swap;
+    }
+
+    double mmul_end = MPI_Wtime();
+
+    delete[] task_result;
+    delete[] recvbuf;
+    delete[] task_results;
+    delete[] sendbuf;
+    delete[] x0_vec;
 
     if (rank == 0) {
-        delete[] sendbuf;
+        printf("Result: %d\n", result[0]);
+        printf("Time: %.6f ms\n", mmul_end - mmul_start);
+        printf("\n");
+        if (result[0] == seq_result) {
+            printf("Results equal\n");
+        } else {
+            fprintf(stderr, "Results not equal");
+        }
     }
+    delete[] result;
+
+    MPI_Finalize();
 #endif
     return 0;
 }
--- myutils.hpp
+++ myutils.hpp
@@ -7,54 +7,12 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
-#include <Eigen/Dense>
+#include <functional>
 
 using std::vector;
-template<typename T>
-using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 
 vector<int> count_indices(int len, int task_cnt);
 vector<int> count_displ(int len, int task_cnt);
-
-template<typename T>
-bool check_result(const vector<T> &vec, T x0, T result) {
-    return (std::accumulate(vec.begin(),  vec.end(), 1, std::multiplies<>()) * x0) == result;
-}
-
-// Returns matrix of size matrix_dim x (n*matrix dim)
-template<typename T>
-Matrix<T> generate_matrix(int n, int matrix_dim, const vector<T>& coefficients) {
-    Matrix<T> result(matrix_dim, matrix_dim*n);
-    result.setZero();
-    std::copy(coefficients.begin(), coefficients.end(), result.row(0).begin());
-    for(int i = 1; i < matrix_dim - 1; ++i) {
-        for (int j = 0; j < matrix_dim*n; j += matrix_dim) {
-            result(i, j + i - 1) = 1;
-        }
-    }
-    for (int j = matrix_dim - 1; j < matrix_dim*n; j += matrix_dim) {
-        result(matrix_dim - 1, j) = 1;
-    }
-    return result;
-}
-
-template<typename T>
-vector<Matrix<T>> generate_matrices(int n, int matrix_dim, const vector<T>& coefficients) {
-    vector<Matrix<T>> result(n, Matrix<T>(matrix_dim, matrix_dim));
-    for(int i = 0; i < n; ++i) {
-        auto &matrix = result[i];
-        auto first_row = matrix.row(0);
-        std::copy(coefficients.begin() + (matrix_dim*i), coefficients.begin() + (matrix_dim*(i+1)), first_row.begin());
-        auto zeros = matrix.block(1, 0, matrix_dim - 1, matrix_dim).reshaped();
-        std::fill(zeros.begin(), zeros.end(), 0);
-        matrix(matrix_dim - 1, matrix_dim - 1) = 1;
-        if (matrix_dim != 2) {
-            auto diag = matrix.block(1, 0, matrix_dim - 2, matrix_dim - 2).diagonal();
-            std::fill(diag.begin(), diag.end(), 1);
-        }
-    }
-    return result;
-}
 
 template<typename T>
 T* generate_matrices(int n, int matrix_dim, const T* coefficients) {
@@ -82,7 +40,7 @@ vector<T> generate_coefficients(int n, int coeff_num, std::function<T (void)>&& 
 }
 
 template<typename T>
-T* gemm_v1(int M, int N, int K, const T *A, const T *B)
+T* gemm(int M, int N, int K, const T *A, const T *B)
 {
     T* C = new T[M*K];
     for (int i = 0; i < M; ++i)
@@ -100,6 +58,5 @@ T* gemm_v1(int M, int N, int K, const T *A, const T *B)
     }
     return C;
 }
-
 
 #endif //PARALG_CPP_MYUTILS_HPP
